801296c75fbf8c821affc17651a8561c1406a3d6
wjmccann <wjmccann@live.com.au>
2017-01-28 19:07:30 +1100

    remote image fix

--- RSL/urls.py
+++ RSL/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.static import static
 
+from django.conf import settings
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 	url(r'^museum/', include('museum.urls')),
 	url(r'^', include('museum.urls')),
 	url(r'^nested_admin/', include('nested_admin.urls')),
-] 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
